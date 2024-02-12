18f1473168f20270766ebaf8f2145dfd667b1872
guledali <guled.ali01@gmail.com>
2024-02-12 11:52:42 +0100

    konfig vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Must be 'static' or 'hybrid'
+  output: "static",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+});
